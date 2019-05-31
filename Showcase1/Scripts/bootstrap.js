20508edfb377dff7c292a8e6bebdc2ab58620ea6
William <williamboston123@hotmail.com>
2019-05-30 17:23:01 -0700

    Startup

--- Showcase1/Scripts/bootstrap.js
+++ Showcase1/Scripts/bootstrap.js
@@ -2375,3 +2375,15 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("navbar-shrink").style.padding = "30px 10px";
+        
+    } else {
+        document.getElementById("navbar-shrink").style.padding = "80px 10px";
+    }
+} 
